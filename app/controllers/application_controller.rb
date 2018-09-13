be2cf01961c019dff690f681235264e5b489443b
Alexander Ilchukov <alex.ilchukov@gmail.com>
2018-09-13 06:59:45 +0300

    #5 Add support of current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@
 
 # Base class of controller classes
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :reset_session
+
+  # Returns record of user with identifier stored in session or `nil` if there
+  # is no stored identifier or the record can't be found
+  # @return [User]
+  #   record of user with identifier stored in session
+  # @return [NilClass]
+  #   if there is no stored identifier in session or record of user can't be
+  #   found by it
+  def current_user
+    return @current_user if defined?(@current_user)
+    user_id = session[:user_id]
+    @current_user = user_id && User.find(user_id)
+  end
+
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root to: 'sessions#new'
 
-  get 'users/new' => 'users#new'
+  get 'users/new' => 'users#new', as: :new_user
   post 'users' => 'users#create'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
