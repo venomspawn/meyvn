754c0f5849dc5c5e2be0f6e5842209a313167c6f
Alexander Ilchukov <alex.ilchukov@gmail.com>
2018-09-16 09:47:50 +0300

    #9 Add database migration which creates table of events

--- db/structure.sql
+++ db/structure.sql
@@ -63,6 +63,23 @@ CREATE TABLE public.cities (
 
 
 --
+-- Name: events; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.events (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    title text NOT NULL,
+    place text NOT NULL,
+    start timestamp without time zone NOT NULL,
+    finish timestamp without time zone NOT NULL,
+    city_id uuid NOT NULL,
+    topic_id uuid NOT NULL,
+    creator_id uuid NOT NULL,
+    CONSTRAINT events_check_start_is_less_than_finish CHECK ((start < finish))
+);
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -110,6 +127,14 @@ ALTER TABLE ONLY public.cities
 
 
 --
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -141,6 +166,34 @@ CREATE UNIQUE INDEX cities_lower_name_unique_key ON public.cities USING btree (l
 
 
 --
+-- Name: index_events_on_city_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_events_on_city_id ON public.events USING btree (city_id);
+
+
+--
+-- Name: index_events_on_creator_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_events_on_creator_id ON public.events USING btree (creator_id);
+
+
+--
+-- Name: index_events_on_start; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_events_on_start ON public.events USING btree (start);
+
+
+--
+-- Name: index_events_on_topic_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_events_on_topic_id ON public.events USING btree (topic_id);
+
+
+--
 -- Name: topics_lower_title_unique_key; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -155,6 +208,30 @@ CREATE UNIQUE INDEX users_lower_email_unique_key ON public.users USING btree (lo
 
 
 --
+-- Name: events fk_rails_07b11fc6bb; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT fk_rails_07b11fc6bb FOREIGN KEY (city_id) REFERENCES public.cities(id);
+
+
+--
+-- Name: events fk_rails_15c34a9137; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT fk_rails_15c34a9137 FOREIGN KEY (creator_id) REFERENCES public.users(id);
+
+
+--
+-- Name: events fk_rails_e5e78194cb; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT fk_rails_e5e78194cb FOREIGN KEY (topic_id) REFERENCES public.topics(id);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
@@ -164,6 +241,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180908124326'),
 ('20180908125414'),
 ('20180914062139'),
-('20180916081935');
+('20180916081935'),
+('20180916092217');
 
 
