867ef7d518f8f236cde386594155ac37ef95f1f2
Alexander Ilchukov <alex.ilchukov@gmail.com>
2018-10-10 17:33:07 +0300

    #38 Add parentheses to nested calls

--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -4,22 +4,22 @@ require 'rails_helper'
 
 RSpec.describe 'Routing for events REST API', type: :routing do
   it 'routes POST /events to events/creation#create' do
-    expect(post '/events')
+    expect(post('/events'))
       .to route_to(controller: 'events/creation', action: 'create')
   end
 
   it 'routes GET /events to events/index_page#draw' do
-    expect(get '/events')
+    expect(get('/events'))
       .to route_to(controller: 'events/index_page', action: 'draw')
   end
 
   it 'routes GET /events/new to events/new_page#draw' do
-    expect(get '/events/new')
+    expect(get('/events/new'))
       .to route_to(controller: 'events/new_page', action: 'draw')
   end
 
   it 'routes GET /events/stream to events/stream#stream' do
-    expect(get '/events/stream')
+    expect(get('/events/stream'))
       .to route_to(controller: 'events/stream', action: 'stream')
   end
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,32 +4,32 @@ require 'rails_helper'
 
 RSpec.describe 'Routing for user management REST API', type: :routing do
   it 'routes GET /users/new to users/new_page#draw' do
-    expect(get '/users/new')
+    expect(get('/users/new'))
       .to route_to(controller: 'users/new_page', action: 'draw')
   end
 
   it 'routes POST /users to users/creation#create' do
-    expect(post '/users')
+    expect(post('/users'))
       .to route_to(controller: 'users/creation', action: 'create')
   end
 
   it 'routes GET /login to users/login_page#draw' do
-    expect(get '/login')
+    expect(get('/login'))
       .to route_to(controller: 'users/login_page', action: 'draw')
   end
 
   it 'routes POST /login to users/login#login' do
-    expect(post '/login')
+    expect(post('/login'))
       .to route_to(controller: 'users/login', action: 'login')
   end
 
   it 'routes POST /logout to users/logout#logout' do
-    expect(post '/logout')
+    expect(post('/logout'))
       .to route_to(controller: 'users/logout', action: 'logout')
   end
 
   it 'routes POST /save_filter to users/filter_saving#save' do
-    expect(post '/save_filter')
+    expect(post('/save_filter'))
       .to route_to(controller: 'users/filter_saving', action: 'save')
   end
 end
